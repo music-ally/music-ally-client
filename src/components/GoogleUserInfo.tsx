9e5f878b4c34da32b73cff35fb738c88dc973c61
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-05-22 20:54:11 +0900

    Feat: 로그인 정보 쿠키에 저장
    추후 백엔드로 쿠키 정보 전달 가능하도록

--- src/components/GoogleUserInfo.tsx
+++ src/components/GoogleUserInfo.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 import { useEffect, useState } from "react";
 
 interface UserInfo {
@@ -7,31 +8,35 @@ interface UserInfo {
     profile? : string;
 }
 
-export default function GoogleUserInfo({ accessToken } : { accessToken: string}){
+export default function GoogleUserInfo(){
     const [ userInfo, setUserInfo ] = useState<UserInfo>({});
 
     useEffect(() => {
-        const getUserInfo = async() => {
-            try {
-                const response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                });
-                setUserInfo({
-                    name: response.data.name,
-                    email: response.data.email,
-                    profile: response.data.profile,
-                });
+        const accessToken = Cookies.get('access_token');
 
-                console.log('UserEmail : ', response.data.email);
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        getUserInfo();
-    }, [accessToken]);
+        if (accessToken) {
+            const getUserInfo = async() => {
+                try {
+                    const response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+                        headers: {
+                            'Authorization': `Bearer ${accessToken}`
+                        }
+                    });
+                    setUserInfo({
+                        name: response.data.name,
+                        email: response.data.email,
+                        profile: response.data.picture,
+                    });
+    
+                    console.log('UserEmail : ', response.data.email);
+                    console.log(response.data);
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+            getUserInfo();
+        }
+    }, []);
 
     return (
         <div>
