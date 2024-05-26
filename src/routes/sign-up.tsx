26f07419a8606adf2797598dba6c166165115b0a
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-05-27 00:56:13 +0900

    Feat: 생년월일 드롭다운

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -30,6 +30,43 @@ const Divider = styled.div`
     align-items: center; /* 가운데 정렬 */
 `
 
+const Select = styled.select`
+    padding: 15px;
+    border-radius: 10px;
+    width: 30%;
+    font-size: 16px;
+    background-color: black;
+    color: #BFBFBF;
+    box-sizing: border-box; // padding 조절하여도 너비 그대로
+    border: 1px solid #ffffff; 
+`
+
+const YearSelect = ()  => (
+    <Select>
+        <option value="">Year</option>
+        {Array.from( { length:100 }, (_, i) => new Date().getFullYear() - i).map(year => (
+            <option key={year} value={year}>{year}</option>
+        ))}
+    </Select>
+);
+
+const MonthSelect = ()  => (
+    <Select>
+        <option value="">Month</option>
+        {Array.from( { length:12 }, (_, i) => i + 1).map(month => (
+            <option key={month} value={month}>{month}</option>
+        ))}
+    </Select>
+);
+
+const DaySelect = ()  => (
+    <Select>
+        <option value="">Day</option>
+        {Array.from( { length:31 }, (_, i) => i + 1).map(day => (
+            <option key={day} value={day}>{day}</option>
+        ))}
+    </Select>
+);
 
 export default function SignUp() {
     const [email, setEmail] = useState("");
@@ -131,6 +168,13 @@ export default function SignUp() {
                         </Text>
                         {/* 구분선 */}
                         <Divider/>
+                        {/* 생년월일 입력 */}
+                        <Name> 생년월일 </Name>
+                        <Row1>
+                            <YearSelect />
+                            <MonthSelect />
+                            <DaySelect />
+                        </Row1>
                     </Form>
                 </RightWrapper>
             </RightHalf>
