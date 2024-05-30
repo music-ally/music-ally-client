0d1d6394c5f8a5514394d9b8e7428181310dc408
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-05-30 15:46:27 +0900

    Feat: 홍보 배너

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,24 +1,27 @@
-import { useLocation } from "react-router-dom";
-import GoogleUserInfo from "../components/GoogleUserInfo";
-import { useEffect } from "react";
-import Cookies from 'js-cookie';
+import { styled } from "styled-components";
+// import { Wrapper } from "../components/auth-components";
+import Banner from "../components/banner";
+
+const Parent = styled.div`
+    height: 100vh;  // 부모 요소가 화면 전체 높이를 차지하도록 설정
+    display: flex;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 1280px;
+    min-width: 1280px;
+`
 
 export default function Home() {
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const accessToken = queryParams.get('access_token');
-
-    useEffect(() => {
-        if (accessToken) {
-            Cookies.set('access_token', accessToken, {expires: 1});
-        }
-    }, [accessToken]);
-
     return (
-        <>
-            <h1> 로그인 테스트 </h1>
-            {/* {accessToken && <GoogleUserInfo accessToken = {accessToken}/>} */}
-            <GoogleUserInfo />
-        </>
+        <Parent>
+            <Wrapper>
+                <Banner />
+            </Wrapper>
+        </Parent>
     );
 }
