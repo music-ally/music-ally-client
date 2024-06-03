2251e7b6aa96c0ec8ddd80daab588d9ca19d2279
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-06-04 01:40:07 +0900

    Feat: 구글이메일 정보 회원가입창으로
    구글로그인 버튼 클릭 시 해당 이메일 정보 회원가입 창에 자동 입력. 추후 if else문 이용하여 이메일 정보 존재하면 바로 로그인, 존재하지 않으면 sns 가입 창으로 이동

--- src/components/google-btn.tsx
+++ src/components/google-btn.tsx
@@ -38,24 +38,37 @@ export default function GoogleButton(){
     };
 
     const login = useGoogleLogin({
-        onSuccess: (credentialResponse) => {
+        onSuccess: async (credentialResponse) => {
             try {
                 console.log(credentialResponse.access_token);
                 // 쿠키에 토큰 저장
                 Cookies.set('access_token', credentialResponse.access_token, {expires: 1}); // 1일동안 유효
-                navigate("/");
-                
-                // // 추후 백엔드에 쿠키를 포함한 요청 보내기 코드
-                // axios.get('http://백엔드도메인.com/api/protected', {
-                //     withCredentials: true // 쿠키 포함 요청
-                // })
-                // .then(response => {
-                //     console.log('Protected data: ', response.data);
-                //     navigate("/"); //요청 성공 후 홈으로 redirection
-                // })
-                // .catch(error => {
-                //     console.error('Error fetching protected data: ', error);
-                // })
+                // navigate("/");
+
+                // 토큰으로 사용자 정보 가져오기
+                const {data} = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+                    headers: {
+                        Authorization: `Bearer ${credentialResponse.access_token}`,
+                    },
+                });
+
+                // 일단 지금은 백엔드 소통 않고 받은 이메일 정보 직접 이동 처리
+                navigate("/sns-signup", {state: {email: data.email}});
+
+
+            /*
+                // 추후 백엔드에 쿠키를 포함한 요청 보내기 코드
+                axios.get('http://백엔드도메인.com/api/protected', {
+                    withCredentials: true // 쿠키 포함 요청
+                })
+                .then(response => {
+                    console.log('Protected data: ', response.data);
+                    navigate("/"); //요청 성공 후 홈으로 redirection
+                })
+                .catch(error => {
+                    console.error('Error fetching protected data: ', error);
+                })
+            */
 
             } catch (error) {
                 console.log("로그인 성공, 그러나 토큰 디코딩 에러:", error);
