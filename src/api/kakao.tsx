fec6fa6b3ebb61f1f6b9838f794147160a5eb966
stern <psm1st@sookmyung.ac.kr>
2024-06-03 12:34:30 +0900

    fix : 초기화 error catch 구문 작성

--- src/api/kakao.tsx
+++ src/api/kakao.tsx
@@ -1,36 +1,68 @@
-import React from "react";
-import KakaoLogin from "react-kakao-login";
-import axios from "axios";
+import React, { useEffect } from "react";
 import KakaoButton from "../components/kakao-btn";
 
-const KakaoOauth: React.FC=()=>{
-    const handleSuccess = async(response:any)=>{
-        const {code} =response;
-        try{
-            // const res=await axios.post('http://localhost:3000/login',{code})
-            // console.log('Access Token:',res.data.access_token);
-            window.location.href = "http://localhost:3000";
-        }catch(error){
-            console.error(error);
+declare global{
+    interface Window{
+        Kakao: any;
+    }
+}
+
+const Kakao: React.FC = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
+        script.onload = () => {
+            const javascriptKey = import.meta.env.VITE_kakao_JAVASCRIPT_KEY;
+            if (window.Kakao && !window.Kakao.isInitialized()) {
+                window.Kakao.init(javascriptKey);
+            }
+        };
+        document.head.appendChild(script);
+        
+        if (window.Kakao && !window.Kakao.isInitialized()) {
+            const javascriptKey = import.meta.env.VITE_JAVASCRIPT_KEY;
+            window.Kakao.init(javascriptKey);
+        }
+    }, []);
+  
+    const loginKakao = () => {
+        if (window.Kakao && window.Kakao.Auth) {
+            window.Kakao.Auth.login({
+                redirectUri: 'http://localhost:3000',
+                success: (authObj: any) => {
+                    console.log('로그인 성공', authObj);
+                    getUserInfo();
+                },
+                fail: (err: any) => {
+                    console.error('로그인 실패', err);
+                }
+            });
+        } else {
+            console.error('Kakao 객체가 초기화되지 않았습니다.');
         }
     };
 
-    const handleFailure=(error:any)=>{
-        console.error(error);
+    const getUserInfo = () => {
+        if (window.Kakao && window.Kakao.API) {
+            window.Kakao.API.request({
+                url: 'v2/user/me',
+                success: (res: any) => {
+                    console.log(res);
+                },
+                fail: (err: any) => {
+                    console.error(err);
+                }
+            });
+        } else {
+            console.error('Kakao 객체가 초기화되지 않았습니다.');
+        }
     };
 
-    return(
-        <KakaoLogin
-            token={import.meta.env.VITE_REST_API_KEY}
-            onSuccess={handleSuccess}
-            onFail={handleFailure}
-            render={
-                (props)=>(
-                    <KakaoButton onClick={props.onClick}/>
-                )
-            }
-        />
-    )
-}
+    return (
+        <div>
+            <KakaoButton onClick={loginKakao} />
+        </div>
+    );
+};
 
-export default KakaoOauth;
+export default Kakao;
