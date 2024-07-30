a91aa5930c94171eb6dfc98dc238d04062e4655f
jjwm10625 <152948170+jjwm10625@users.noreply.github.com>
2024-07-31 02:30:28 +0900

    Feat: 배너 이미지 추가

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,73 +1,72 @@
-import styled from 'styled-components';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import styled from "styled-components";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const BannerContainer = styled.div`
-    width: 100%;
-    height: 400px;
-    overflow: hidden;
-`
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+`;
 
 const CustomSwiperWrapper = styled.div`
-    .swiper-pagination {
-        position: absolute;
-        left: 10px;
-        bottom: 15px;
-        display: flex;
-        gap: 5px;
-    }
-    
-    /* .swiper-pagination-bullet { // 선택 안된
+  .swiper-pagination {
+    position: absolute;
+    left: 10px;
+    bottom: 15px;
+    display: flex;
+    gap: 5px;
+  }
+
+  /* .swiper-pagination-bullet { // 선택 안된
         background-color: black;
     } */
-    
-    .swiper-pagination-bullet-active { // 현재 선택
-        background-color: white;
-    }
+
+  .swiper-pagination-bullet-active {
+    // 현재 선택
+    background-color: white;
+  }
 `;
 
 const SlideImage = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 export default function Banner() {
-    return(
-        <BannerContainer>
-            <CustomSwiperWrapper>
-                <Swiper
-                    modules={[Autoplay, Pagination, Navigation]}
-                    spaceBetween={0}
-                    slidesPerView={1}
-                    autoplay={{
-                        delay: 2000,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={{ clickable: true }}
-                    loop={true}
-                >
-                    <SwiperSlide>
-                        <SlideImage src="/banner1.png" alt="Slide 1" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideImage src="https://via.placeholder.com/1280x400?text=Slide+2" alt="Slide 2" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideImage src="https://via.placeholder.com/1280x400?text=Slide+3" alt="Slide 3" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideImage src="https://via.placeholder.com/1280x400?text=Slide+4" alt="Slide 4" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideImage src="/banner5.png" alt="Slide 5" />
-                    </SwiperSlide>
-                </Swiper>
-            </CustomSwiperWrapper>
-            
-
-        </BannerContainer>
-    );
+  return (
+    <BannerContainer>
+      <CustomSwiperWrapper>
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          loop={true}
+        >
+          <SwiperSlide>
+            <SlideImage src="/banner1.png" alt="Slide 1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SlideImage src="/banner2.png" alt="Slide 2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SlideImage src="/banner3.png" alt="Slide 3" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SlideImage src="/banner4.png" alt="Slide 4" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SlideImage src="/banner5.png" alt="Slide 5" />
+          </SwiperSlide>
+        </Swiper>
+      </CustomSwiperWrapper>
+    </BannerContainer>
+  );
 }
