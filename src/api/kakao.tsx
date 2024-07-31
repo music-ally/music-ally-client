c05cffdb06e8cec9283ae895f396a32191c90def
psm1st <145013061+psm1st@users.noreply.github.com>
2024-07-31 21:49:33 +0900

    fix : 백엔드랑 연결하고 수정 중

--- src/api/kakao.tsx
+++ src/api/kakao.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import KakaoButton from "../components/kakao-btn";
+import axios from "axios";
+import { redirect, useNavigate } from "react-router-dom";
 
-declare global{
-    interface Window{
+declare global {
+    interface Window {
         Kakao: any;
     }
 }
 
 const Kakao: React.FC = () => {
+    const navigate = useNavigate();
+
     useEffect(() => {
         const script = document.createElement('script');
         script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
@@ -18,41 +22,53 @@ const Kakao: React.FC = () => {
             }
         };
         document.head.appendChild(script);
-        
-        if (window.Kakao && !window.Kakao.isInitialized()) {
-            const javascriptKey = import.meta.env.VITE_KAKAO_JAVASCRIPT_API;
-            window.Kakao.init(javascriptKey);
-        }
     }, []);
-  
-    const loginKakao = () => {
-        if (window.Kakao && window.Kakao.Auth) {
-            window.Kakao.Auth.login({
-                redirectUri: 'http://localhost:3000',
-                success: (authObj: any) => {
-                    console.log('로그인 성공', authObj);
-                    localStorage.setItem('kakaoToken', authObj.access_token);
-                    getUserInfo();
-                },
-                fail: (err: any) => {
-                    console.error('로그인 실패', err);
-                }
-            });
-        } else {
-            console.error('Kakao 객체가 초기화되지 않았습니다.');
+
+    const checkEmailExistence = async (email: string) => {
+        try {
+            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/check/email`, { email });
+            return response.data.exists;
+        } catch (error) {
+            console.error('Email check error:', error);
+            return false;
         }
     };
 
-    const getUserInfo = () => {
-        if (window.Kakao && window.Kakao.API) {
-            window.Kakao.API.request({
-                url: 'v2/user/me',
-                success: (res: any) => {
-                    console.log(res);
+    const loginKakao = () => {
+        if (window.Kakao && window.Kakao.Auth) {
+            window.Kakao.Auth.login({
+                redirectUri : 'http://192.168.219.168:3000/home',
+                success: async (authObj: any) => {
+                    console.log('로그인 성공', authObj);
+                    localStorage.setItem('access_token', authObj.access_token);
+                    localStorage.setItem('kakaoRefreshToken', authObj.refresh_token);
+                    if (window.Kakao && window.Kakao.API) {
+                        window.Kakao.API.request({
+                            url: '/v2/user/me',
+                            success: async (res: any) => {
+                                const email = res.kakao_account.email;
+                                const sub = res.id;
+
+                                if (email) {
+                                    const emailExists = await checkEmailExistence(email);
+                                    if (emailExists) {
+                                        alert('이미 가입된 이메일입니다. 이메일 로그인을 시도해주세요.');
+                                        navigate('/login');
+                                    } else {
+                                        navigate('/sns-signup', { state: { email, social_id: sub } });
+                                    }
+                                } else {
+                                }
+                            },
+                            fail: (err: any) => {
+                                console.error('사용자 정보 요청 실패', err);
+                            }
+                        });
+                    }
                 },
                 fail: (err: any) => {
-                    console.error(err);
-                }
+                    console.error('로그인 실패', err);
+                },
             });
         } else {
             console.error('Kakao 객체가 초기화되지 않았습니다.');
@@ -63,7 +79,7 @@ const Kakao: React.FC = () => {
         <div>
             <KakaoButton onClick={loginKakao} />
         </div>
-    );      
+    );
 };
 
 export default Kakao;
--- src/routes/myprofile.tsx
+++ src/routes/myprofile.tsx
@@ -188,7 +188,6 @@ export default function MyPage() {
             try {
                 //const accessToken = Cookies.get("access_token"); // 쿠키에서 access_token 가져오기
                 const accessToken = localStorage.getItem("access_token"); // 로컬 스토리지에서 access_token 가져오기
-
                 const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/myPage`, {
                     headers: {
                         Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 포함
